c26614c6a980bdb12fc9677d04c8a1f66cf3e8fd
TebatsoSeshayi <TebatsoSeshayiOut@outlook.com>
2026-01-30 01:37:54 +0200

    Initialize Maven project with basic structure and sample classes

---
+++ src/main/java/io/pulseds/App.java
@@ -0,0 +1,13 @@
+package io.pulseds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
